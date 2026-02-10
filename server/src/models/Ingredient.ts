3a609dba60aece8b8eda736984398afca3549a2d
Amélie <amelie.beaufils95@gmail.com>
2026-02-10 11:25:14 +0100

    feat : add ingredient

---
+++ server/src/models/Ingredient.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IIngredient extends Document {
+  name: string;
+  category: string;
+}
+
+const ingredientSchema = new Schema<IIngredient>({
+  name: {
+    type: String,
+    required: [true, "Le nom de l'ingrédient est requis"],
+    unique: true,
+    trim: true,
+    minlength: [2, "Le nom doit contenir au moins 2 caractères"],
+  },
+  category: {
+    type: String,
+    required: [true, "La catégorie de l'ingrédient est requise"],
+    enum: [
+      "légume",
+      "viande",
+      "poisson",
+      "épice",
+      "produit laitier",
+      "féculent",
+      "autre",
+    ],
+  },
+});
+
+export default mongoose.model<IIngredient>("Ingredient", ingredientSchema);
